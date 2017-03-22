9c7903890f626631715873dc77e8b88825b7c98b
leichaofan <leichaofan2024@gmail.com>
2017-03-22 18:17:59 +0800

    change admin/product backend panel

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -7,7 +7,7 @@ class Admin::ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find([:id])
+    @product = Product.find(params[:id])
   end
   def new
     @product = Product.new
